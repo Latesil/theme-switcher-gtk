e42b92e64be9ab5823efddae65b4314e16d17fa6
latesil <skipastas@gmail.com>
2019-06-26 23:49:45 +0300

    add auto-switch-toggle

--- window.py
+++ window.py
@@ -1,4 +1,5 @@
 import gi
+import subprocess
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
@@ -18,16 +19,15 @@ class MyWindow(Gtk.Window):
         self.box = Gtk.Box(spacing=6)
         self.add(self.box)
         
-        #change to the grid for filechooser
+        #TODO change to the grid for filechooser
         self.button1 = Gtk.Button(label="Hello")
         self.button1.connect("clicked", self.on_button1_clicked)
         self.box.pack_start(self.button1, True, True, 0)
 
+        #TODO change to the grid time managment
         self.button2 = Gtk.Button(label="Goodbye")
         self.button2.connect("clicked", self.on_button2_clicked)
         self.box.pack_start(self.button2, True, True, 0)
-
-        
 
         button = Gtk.ToggleButton(label="Auto Switch")
         button.connect("toggled", self.on_button_clicked, "1")
@@ -38,10 +38,18 @@ class MyWindow(Gtk.Window):
 
     def on_button2_clicked(self, widget):
         print("Goodbye")
+        
+    def state_off(self):
+        subprocess.call(['systemctl','stop','--user','theme-switcher-auto.timer'])
+        subprocess.call(['systemctl','disable','--user','theme-switcher-auto.timer'])
+        
+    def state_on(self):
+        subprocess.call(['systemctl','start','--user','theme-switcher-auto.timer'])
+        subprocess.call(['systemctl','enable','--user','theme-switcher-auto.timer'])
                 
     def on_button_clicked(self, button, name):
         if button.get_active():
-            state = "on"
+            self.state_on()
         else:
-            state = "off"
-        print("Button", name, "was turned", state)
+            self.state_off()
+        #print("Button", name, "was turned", state)
